d24e147514c99abad93d454a4796f6841405b9a7
sutharjay1 <sutharjay3635@gmail.com>
2024-11-05 15:34:00 +0530

    fix(app): Setting route props fix

---
+++ src/app/(main)/dashboard/[workspaceId]/settings/general/page.tsx
@@ -0,0 +1,7 @@
+import Container from "@/app/(main)/_components/container";
+
+const Settings = () => {
+  return <Container showItems={false}>Settings</Container>;
+};
+
+export default Settings;
--- src/app/(main)/dashboard/[workspaceId]/settings/page.tsx
+++
@@ -1,7 +0,0 @@
-import Container from "../../../_components/container";
-
-const Settings = () => {
-  return <Container>Settings</Container>;
-};
-
-export default Settings;
